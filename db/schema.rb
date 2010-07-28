a08bf48e4257d5209be71e56a09ecaedf47cc7c8
Erik M Jacobs <erikmjacobs@gmail.com>
2010-07-28 17:04:32 -0400

    integer, not int, you silly person

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100728205357) do
+ActiveRecord::Schema.define(:version => 20100728210400) do
 
   create_table "toilets", :force => true do |t|
     t.string   "street_address"
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+    t.integer  "rating"
     t.float    "lat"
     t.float    "lng"
     t.datetime "created_at"
