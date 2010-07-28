d67fb4ae8cf47a2743f006ab9e15dadfaaaf4e6f
Erik M Jacobs <erikmjacobs@gmail.com>
2010-07-28 17:14:15 -0400

    configured toilet model

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,3 +1,9 @@
 class Toilet < ActiveRecord::Base
   attr_accessible :street_address, :city, :state, :zip, :rating, :lat, :lng
+  acts_as_mappable :auto_geocode => true
+
+  def address
+    return "#{self.street_address}, #{self.city}, #{self.state}, #{self.zip}"
+  end
 end
+
